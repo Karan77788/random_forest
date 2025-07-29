1bf4c3ac5238595225668d6098750f79da57f4ed
Karan77788 <karanbalamurugan7@gmail.com>
2025-07-29 09:52:48 +0530

    updated file

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def index():
 
     return render_template('index.html', prediction=prediction)
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Render sets the PORT variable
     app.run(host='0.0.0.0', port=port)
